d1b34fcd59c4bd96ad5c339be0c0584ab9c15c81
ConstantLrnr <155127455+ConstantLrnr@users.noreply.github.com>
2024-08-20 11:14:21 -0400

    Add files via upload

---
+++ BMI_Calculator.py
@@ -0,0 +1,24 @@
+height= input()    #enter in meters e.g: 1.65
+weight= input()    #enter in kilograms e.g 89
+
+height_as_float = float(height)
+weight_as_int = int(weight)     # whole number
+
+#using the exponent operater **
+
+BMI= weight_as_int/height_as_float ** 2
+
+# or using multiplication or PEMDAS
+
+BMI= weight_as_int/ (height_as_float * height_as_float)
+
+
+BMI_as_int = int(BMI)
+print(BMI_as_int)
+
+
+
+
+ 
+
+
