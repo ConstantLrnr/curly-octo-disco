e1d91e3c3960aed51e143e310de012016a28daf6
ConstantLrnr <155127455+ConstantLrnr@users.noreply.github.com>
2025-01-20 21:01:35 -0500

    Add files via upload

---
+++ words_list.py
@@ -0,0 +1,14 @@
+words = ["Cache","ROM","DNS","IP address","Cloud computing",
+"Algorithm","Debug","Interface","Byte","Bit","Modem","Browser","Cookie",
+"Patch","Encryption","Database","Server","Client","DOS","RAID","DDoS","SMTP",
+"URL","HTML","CSS","JavaScript","TCP/IP","API","Bandwidth","Cloud","Firewall",
+"GUI","Hardware","Kernel","Malware","Network","OS","Pixel","RAM","Router",
+"Software","Trojan","VPN","Web browser","Wireless"]
+
+
+
+
+# import words_list #imports module
+# words = words_list.words #access list of words module
+# duplicates = list(set([word for word in words if words.count(word)>1])) #checks to see if word appears more than once
+# print("Duplicates:", duplicates) #removes dups entries in the list
